f94d0a424239203a14294d181c6070208777b98b
Henry Rovnyak <h.rovnyak@proton.me>
2025-12-24 11:24:40 -0500

    feat: API boundary for the CV code

---
+++ src/hungarian_algorithm.rs
@@ -0,0 +1,6 @@
+/// Reorder the members of `items` such that the cost of placing each member at each index is minimized.
+///
+/// `costs[i][j]` is the cost of placing `items[i]` at index `j`
+fn minimum_matching<I>(items: &mut [I], costs: Vec<Vec<Option<f64>>>) {
+    todo!()
+}
