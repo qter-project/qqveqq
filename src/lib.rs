20af15a1f5928c667adcd1d6f584945bbc19ae88
Henry Rovnyak <h.rovnyak@proton.me>
2025-12-28 20:16:58 -0500

    refactor: Simplify the API boundary to avoid threading

--- src/lib.rs
+++ src/lib.rs
@@ -2,61 +2,30 @@ use std::sync::{Arc, mpsc};
 
 use puzzle_theory::{permutations::Permutation, puzzle_geometry::PuzzleGeometry};
 
-mod hungarian_algorithm;
+mod puzzle_matching;
 
 /// Processes images for computer vision
 pub struct CVProcessor {
     puzzle: Arc<PuzzleGeometry>,
     image_size: usize,
-    calibration_tx: mpsc::Sender<(Box<[(f64, f64, f64)]>, Permutation)>,
-    calibration_rx: Option<mpsc::Receiver<(Box<[(f64, f64, f64)]>, Permutation)>>,
 }
 
 impl CVProcessor {
     /// Create a new `CVProcessor` that recognizes the given puzzle in images. `image_size` specifies the number of pixels in the image. The CV algorithm does not care about rows and columns.
     pub fn new(puzzle: Arc<PuzzleGeometry>, image_size: usize) -> CVProcessor {
-        let (tx, rx) = mpsc::channel();
-
-        CVProcessor { puzzle, image_size, calibration_tx: tx, calibration_rx: Some(rx) }
+        CVProcessor { puzzle, image_size }
     }
 
     /// Calibrate the CV processor with an image of the puzzle in the given state.
-    ///
-    /// # Panics
-    ///
-    /// This method panics if the calibration thread panicked or was otherwise killed before calling this method.
     pub fn calibrate(&self, image: Box<[(f64, f64, f64)]>, state: Permutation) {
         assert_eq!(self.image_size, image.len());
-        self.calibration_tx.send((image, state)).expect("The calibration thread should not be dropped");
-    }
-
-    /// Return a callback that should be called in a new thread and detached. This thread will continuously process CV data in the background to improve the model. Making this its own thread allows image processing to occur immediately and asynchronously with respect to model improvement.
-    ///
-    /// It is a logic error to not spawn this thread and image processing will not work properly without it.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if called more than once.
-    pub fn calibration_thread(&mut self) -> impl FnOnce() + 'static {
-        let rx = self.calibration_rx.take().expect("The calibration thread to only be started once");
-
-        move || {
-            while let Ok((img, perm)) = rx.recv() {
-                println!("Got a calibration!");
-            }
-        }
+        todo!()
     }
 
     /// Process an image and return the most likely state that the puzzle appears to be in, along with the confidence in the prediction. This is guaranteed to be a valid member of the group.
     ///
-    /// The `calibrate` argument tells whether to allow using this prediction for calibration. Setting this to true allows the model to handle non-stationarity in lighting conditions at the cost of errors potentially weakening the model. We recommend setting this to `false` when validating the CV after calibration, and setting it to `true` during normal use.
-    ///
-    /// # Panics
-    ///
-    /// This method panics if
-    /// - `mask` has never been called, or
-    /// - `calibrate` is `true` and the calibration thread panicked or was otherwise killed before calling this method.
-    pub fn process_image(&self, image: Box<[(f64, f64, f64)]>, calibrate: bool) -> (Permutation, f64) {
+    /// To account for non-stationarities and new lighting conditions, it is recommended to call `calibrate` on the output.
+    pub fn process_image(&self, image: Box<[(f64, f64, f64)]>) -> (Permutation, f64) {
         (Permutation::from_mapping(Vec::new()), 0.)
     }
 
@@ -65,5 +34,7 @@ impl CVProcessor {
     /// Each pixel is configured with a number determining which face of the puzzle it belongs to, such that pixels with the same number correspond to the same face. The boolean parameter determines whether the pixel should be treated as white balance for the given face: `false` means that it is not white balance and `true` means that it is white balance.
     ///
     /// White balance points should be selected such that the face is parallel with the face that it is acting as white balance for.
+    ///
+    /// Masking the image can give better CV results
     pub fn mask(&self, mask: Box<[Option<(u32, bool)>]>) {}
 }
---
+++ src/puzzle_matching/mod.rs
@@ -0,0 +1,1 @@
+mod hungarian_algorithm;
