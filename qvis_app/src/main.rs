494d5d316a6f746a7f58f656b9b4011cfa13840d
Arhan Chaudhary <arhan.ch@gmail.com>
2026-01-05 21:26:55 -0800

    WEBSOCKET WORK

--- qvis_app/src/main.rs
+++ qvis_app/src/main.rs
@@ -1,12 +1,95 @@
-use crate::app::App;
+use axum::{
+    Router,
+    body::Body as AxumBody,
+    extract::{FromRef, Path, RawQuery, State},
+    http::{HeaderMap, Request},
+    response::{IntoResponse, Response as AxumResponse},
+    routing::{get, post},
+};
+use leptos::logging::log;
 use leptos::prelude::*;
-use log::Level;
+use leptos_axum::{AxumRouteListing, LeptosRoutes, file_and_error_handler_with_context};
+use leptos_axum::{
+    generate_route_list_with_exclusions_and_ssg_and_context, handle_server_fns_with_context,
+};
+use leptos_ws::WsSignals;
+use qvis_app::app::*;
+use tokio::net::TcpListener;
 
-mod app;
+#[derive(Clone, FromRef)]
+pub struct AppState {
+    pub server_signals: WsSignals,
+    pub routes: Option<Vec<AxumRouteListing>>,
+    pub options: LeptosOptions,
+}
 
-fn main() {
-    _ = console_log::init_with_level(Level::Debug);
-    console_error_panic_hook::set_once();
+async fn leptos_routes_handler(state: State<AppState>, req: Request<AxumBody>) -> AxumResponse {
+    let state1 = state.0.clone();
+    let options1 = state.clone().0.options.clone();
+    let handler = leptos_axum::render_route_with_context(
+        state.routes.clone().unwrap(),
+        move || {
+            provide_context(state1.options.clone());
+            provide_context(state1.server_signals.clone());
+        },
+        move || shell(options1.clone()),
+    );
+    handler(state, req).await.into_response()
+}
 
-    mount_to_body(App);
+async fn ws_handler(
+    State(state): State<AppState>,
+    _path: Path<String>,
+    _headers: HeaderMap,
+    _query: RawQuery,
+    request: Request<AxumBody>,
+) -> impl IntoResponse {
+    handle_server_fns_with_context(
+        move || {
+            provide_context(state.options.clone());
+            provide_context(state.server_signals.clone());
+        },
+        request,
+    )
+    .await
+}
+
+#[tokio::main]
+async fn main() {
+    let conf = get_configuration(None).unwrap();
+    let leptos_options = conf.leptos_options;
+    let addr = leptos_options.site_addr;
+
+    let server_signals = WsSignals::new();
+    let mut state = AppState {
+        options: leptos_options.clone(),
+        routes: None,
+        server_signals: server_signals.clone(),
+    };
+    let state1 = state.clone();
+    let state2 = state.clone();
+
+    let (routes, _) = generate_route_list_with_exclusions_and_ssg_and_context(
+        || view! { <App /> },
+        None,
+        move || provide_context(state1.server_signals.clone()),
+    );
+    state.routes = Some(routes.clone());
+
+    let app = Router::new()
+        .route("/api/{*fn_name}", post(ws_handler).get(ws_handler))
+        .leptos_routes_with_handler(routes, get(leptos_routes_handler))
+        .fallback(file_and_error_handler_with_context::<AppState, _>(
+            move || provide_context(state2.server_signals.clone()),
+            shell,
+        ))
+        .with_state(state);
+
+    log!("listening on http://{addr}");
+    let listener = TcpListener::bind(&addr)
+        .await
+        .expect("couldn't bind to address");
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
---
+++ qvis_app/src/video.rs
@@ -0,0 +1,60 @@
+use leptos::{
+    logging::{error, log},
+    prelude::*,
+};
+use leptos_use::{
+    FacingMode, UseUserMediaOptions, UseUserMediaReturn, VideoTrackConstraints,
+    use_user_media_with_options,
+};
+
+#[component]
+pub fn Video() -> impl IntoView {
+    let video_ref = NodeRef::<leptos::html::Video>::new();
+    let UseUserMediaReturn {
+        stream,
+        start,
+        enabled,
+        set_enabled,
+        ..
+    } = use_user_media_with_options(
+        UseUserMediaOptions::default()
+            .video(VideoTrackConstraints::default().facing_mode(FacingMode::Environment)),
+    );
+
+    start();
+
+    Effect::new(move |_| {
+        match stream.get() {
+            Some(Ok(s)) => {
+                video_ref.with(|v| {
+                    if let Some(v) = v {
+                        v.set_src_object(Some(&s));
+                    }
+                });
+                return;
+            }
+            Some(Err(e)) => error!("Failed to get media stream: {:?}", e),
+            None => log!("No stream yet"),
+        }
+
+        video_ref.with(|v| {
+            if let Some(v) = v {
+                v.set_src_object(None);
+            }
+        });
+    });
+    
+    view! {
+      <div class="flex flex-col gap-4 text-center">
+        <div>
+          <video node_ref=video_ref controls=false autoplay=true muted=true class="w-auto h-96"></video>
+        </div>
+        <button on:click=move |_| {
+          set_enabled.set(!enabled.get())
+        }>{move || if enabled.get() { "Stop Video" } else { "Start Video" }}</button>
+        <button on:click=move |_| {
+          set_enabled.set(!enabled.get())
+        }>{move || if enabled.get() { "Stop" } else { "Start" }}</button>
+      </div>
+    }
+}
